d230ba603b15e5d5bbaea4306bad96da73ed4e7f
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-03 14:35:28 +0100

    Pass comments array to CommentSection component

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import CommentSection from '../CommentSection/CommentSection'
 
 export default ({ data }) => (
   <div>
     {data.username}
+    <CommentSection comments={data.comments} />
   </div>
 );
