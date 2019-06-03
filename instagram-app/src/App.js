2b000472bdd326c42874b0020572dedac07dce1d
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-03 14:47:45 +0100

    Add SeachBar to App.js

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div>
+      <SearchBar />
       {dummyData.map(data => <div key={data.imageUrl}>
         <PostContainer data={data} />
         </div>)}
