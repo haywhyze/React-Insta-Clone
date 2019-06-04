ed901980a9731b47083087309c5a3c478acfcc2d
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-04 23:38:15 +0100

    convert PostFooter component to class components with local stae

--- instagram-app/src/components/PostContainer/PostFooter.js
+++ instagram-app/src/components/PostContainer/PostFooter.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import pt from 'prop-types';
 
-const PostFooter = ({likes}) => (
-  <div className='post-footer'>
-     <img 
-        className=''
-        alt='love' 
-        src='https://image.flaticon.com/icons/svg/149/149217.svg'
-      />
-      <img 
-        className='invert'
-        alt='chat' 
-        src='https://image.flaticon.com/icons/svg/130/130958.svg'
-      />
-      <h4>{likes} likes</h4>
-  </div>
-);
+class PostFooter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      likes: this.props.likes,
+      liked: false,
+    }
+  }
+
+  likePost = (e) => {
+    e.preventDefault();
+    this.setState({
+      liked: !this.state.liked
+    })
+  }
+
+  render() {
+    return (
+      <div className='post-footer'>
+        <img 
+            className='like-post'
+            onClick={this.likePost}
+            alt='love'
+            src={!this.state.liked ? 
+              `https://image.flaticon.com/icons/svg/149/149217.svg`:
+              `https://image.flaticon.com/icons/svg/148/148836.svg`
+            }
+          />
+          <img 
+            className='invert'
+            alt='chat' 
+            src={`https://image.flaticon.com/icons/svg/130/130958.svg`}
+          />
+          <h4>{this.state.likes} likes</h4>
+      </div>
+    );
+  }
+}
 
 export default PostFooter;
 
