94a3a6b1cc433164ce28a1b6cad70a336a998914
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-04 07:52:00 +0100

    converted App.js into a class component

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: dummyData,
+    }
+  }
+
+  render () {
+    return (
     <div>
       <SearchBar />
       <main className='feed'>
-        {dummyData.map(data => <div key={data.imageUrl}>
+        {this.state.data.map(data => <div key={data.imageUrl}>
           <PostContainer data={data} />
           </div>)}
       </main>
     </div>
   );
+  }
 }
 
 export default App;
