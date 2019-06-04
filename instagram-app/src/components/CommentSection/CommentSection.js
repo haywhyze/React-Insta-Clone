030fa6d3f4071d53591665a4c88cd1bec29b207a
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-04 14:51:43 +0100

    Turn CommentSection component to class component and also added post button to add new component

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import pt from 'prop-types';
 import uuidv1 from 'uuid/v1';
 import moment from 'moment';
 import Comment from './Comment';
 import './Comments.css';
 
-const CommentSection = ({ comments, timestamp }) => (
-  <div className='comments-section'>
-    {comments.map(comment => <div key={uuidv1()}><Comment data={comment}/></div>)}
-    <p className='timestamp'>
-      {moment(timestamp, 'MMMM Do YYYY, h:mm:ss a').fromNow()}
-    </p>
-    <input className='comment-input' type='text' placeholder='Add a Comment...'/>
-  </div>
-);
+class CommentSection extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comments: this.props.comments,
+      newComment: '',
+    }
+  }
+
+  handleComment = (e) => {
+    this.setState({
+      newComment: e.target.value,
+    })
+  }
+
+  render() {
+    return(
+      <div className='comments-section'>
+        {this.state.comments.map(comment => <div key={uuidv1()}><Comment data={comment}/></div>)}
+        <p className='timestamp'>
+          {moment(this.props.timestamp, 'MMMM Do YYYY, h:mm:ss a').fromNow()}
+        </p>
+        <form>
+          <input 
+            className='comment-input' 
+            type='text' 
+            placeholder='Add a Comment...'
+            value={this.state.newComment}
+            onChange={this.handleComment}
+          />
+          <button 
+            disabled={!this.state.newComment.trim() ? true : false}
+          >Post</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default CommentSection;
 
