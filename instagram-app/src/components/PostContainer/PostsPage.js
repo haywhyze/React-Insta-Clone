24be9fa47885652eb6ab5d1081895a50e322c623
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-06 06:10:25 +0100

    remove unnecessary localStorage.clear() funtion

--- instagram-app/src/components/PostContainer/PostsPage.js
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -67,7 +67,6 @@ class PostsPage extends Component {
   }
 
   storeStateInLocalStorage = () => {
-    localStorage.clear();
     localStorage.setItem('instaCloneState', JSON.stringify(this.state))
   }
   
