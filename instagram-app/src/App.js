4cc8a02685feb584206e03371c3f02cd0b60c27d
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-06 03:02:21 +0100

    create ComponentFromWithAuthenticate it's value is set to withAuthentication invoked with PostsPage.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage';
+import withAuthenticate from './components/authentication/withAuthenticate';
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
 
 class App extends Component {
   render () {
     return (
-      <PostsPage />
+      <ComponentFromWithAuthenticate />
     );
   }
 }
