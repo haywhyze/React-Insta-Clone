0ac0721bb563282dd68168638491a17032918f13
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-06 15:33:32 +0100

    Refactored PostFooter to use styled component

--- instagram-app/src/components/PostContainer/PostFooter.js
+++ instagram-app/src/components/PostContainer/PostFooter.js
@@ -1,5 +1,46 @@
 import React, { Component } from 'react';
 import pt from 'prop-types';
+import styled from 'styled-components';
+
+const Postfooter = styled.div`
+  margin: 1rem;
+
+  h4 {
+    margin: .5rem 0;
+  }
+
+  img {
+    cursor: pointer;
+    width: 1.5rem;
+    margin-right: 1rem;
+  }
+
+  .like-post-grow {
+    animation: grow .2s ease-in-out;
+  }
+
+  .like-post {
+    animation: test .2s ease-in-out;
+  }
+
+  @keyframes grow {
+    from {
+        transform: scale(1);
+      }
+    to {
+        transform: scale(1.3);
+      }
+  }
+
+  @keyframes test {
+    from {
+        transform: scale(1);
+      }
+    to {
+        transform: scale(1.2);
+      }
+  }
+`
 
 class PostFooter extends Component {
   constructor(props) {
@@ -20,7 +61,7 @@ class PostFooter extends Component {
 
   render() {
     return (
-      <div className='post-footer'>
+      <Postfooter>
         <img 
             className={!this.state.liked ? 'like-post' : 'like-post-grow'}
             onClick={(e) => this.likePost(e, this.props.index)}
@@ -36,7 +77,7 @@ class PostFooter extends Component {
             src={`https://image.flaticon.com/icons/svg/130/130958.svg`}
           />
           <h4>{this.props.likes} likes</h4>
-      </div>
+      </Postfooter>
     );
   }
 }
