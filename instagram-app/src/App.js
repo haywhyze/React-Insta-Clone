891bade8162210357ae6fe58d2b8a2cf210847c8
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-06 05:22:05 +0100

    add loggedIn state to withAuthenticate HOC and rendered conditionally Login or PostsPage based on loggedIn state

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage';
+import Login from './components/Login/Login';
 import withAuthenticate from './authentication/withAuthenticate';
 
-const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)(Login);
 
 class App extends Component {
   render () {
--- instagram-app/src/authentication/withAuthenticate.js
+++ instagram-app/src/authentication/withAuthenticate.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 
-const withAuthenticate = App => 
+const withAuthenticate = FirstComponent => SecondComponent =>
   class extends Component {
+    constructor() {
+      super();
+
+      this.state = {
+        loggedIn: false,
+      }
+    }
+
+    componentDidMount() {
+      localStorage.username && this.setState({
+        loggedIn: true,
+      })
+    }
+
     render() {
-      return <App />;
+      return this.state.loggedIn ? <FirstComponent /> : <SecondComponent />;
     }
   }
 
